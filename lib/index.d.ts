272e3a6f3a768190daf7bf870440922a91731218
RT <rustytrim11@gmail.com>
2023-09-10 01:27:08 -0400

    New build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,12 @@
 import Client from "./client/Client";
-import WebsocketClient from "./client/WebsocketClient";
+import ClientOptions from "./interfaces/ClientOptions";
 import Constants from "./config/Constants";
+import Event from "./helper/Event";
+import EventHandler from "./util/EventHandler";
 import GatewayIntents from "./config/GatewayIntents";
 import OpCodes from "./config/OpCodes";
-import SolaceIntents from "./config/SolaceIntents";
-import ClientOptions from "./interfaces/ClientOptions";
-import User from "./structures/User";
-import EventHandler from "./util/EventHandler";
 import SolaceError from "./util/SolaceError";
-export { Client, ClientOptions, Constants, EventHandler, GatewayIntents, OpCodes, SolaceError, SolaceIntents, User, WebsocketClient, };
+import SolaceIntents from "./config/SolaceIntents";
+import User from "./structures/User";
+import WebsocketClient from "./client/WebsocketClient";
+export { Client, ClientOptions, Constants, Event, EventHandler, GatewayIntents, OpCodes, SolaceError, SolaceIntents, User, WebsocketClient, };
