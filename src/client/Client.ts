ca90686889ebca64e28a994a149d4e134c657768
RT <rustytrim11@gmail.com>
2023-09-10 00:46:26 -0400

    Client class

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,17 +1,24 @@
 import EventEmitter from "events";
 import ClientOptions from "../interfaces/ClientOptions";
 import WebsocketClient from "./WebsocketClient";
+import GatewayIntents from "../config/GatewayIntents";
+import User from "../structures/User";
 
 export default class Client extends EventEmitter {
 
     private ws: WebsocketClient | null = null;
 
-    constructor(private options: ClientOptions) {
+    public user: User | null = null;
+
+    constructor(public options: ClientOptions) {
         super();
+
+        if (Array.isArray(options.intents)) this.options.intents = GatewayIntents.combineIntents(...options.intents);
+
         this.ws = new WebsocketClient(this);
     }
 
-    public login() {
-        // TODO: LOGIN
+    public async login() {
+        await this.ws?.connect();
     }
 }
