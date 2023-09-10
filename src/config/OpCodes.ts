5cc3f545f392daa068e07246743b9eab20cdbed5
RT <rustytrim11@gmail.com>
2023-09-10 00:47:04 -0400

    OpCodes class added

---
+++ src/config/OpCodes.ts
@@ -0,0 +1,13 @@
+export default class OpCodes {
+    public static DISPATCH = 0;
+    public static HEARTBEAT = 1;
+    public static IDENTIFY = 2;
+    public static PRESENCE_UPDATE = 3;
+    public static VOICE_STATE_UPDATE = 4;
+    public static RESUME = 6;
+    public static RECONNECT = 7;
+    public static REQUEST_GUILD_MEMBERS = 8;
+    public static INVALID_SESSION = 9;
+    public static HELLO = 10;
+    public static HEARTBEAT_ACK = 11;
+}
