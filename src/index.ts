217bf59831708f1f9928d5afa292efed08856eaa
RT <rustytrim11@gmail.com>
2023-09-10 00:46:18 -0400

    Export all classes and interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export const test = "Hello World!";
+import Client from "./client/Client";
+import WebsocketClient from "./client/WebsocketClient";
+import Constants from "./config/Constants";
+import GatewayIntents from "./config/GatewayIntents";
+import OpCodes from "./config/OpCodes";
+import SolaceIntents from "./config/SolaceIntents";
+import ClientOptions from "./interfaces/ClientOptions";
+import User from "./structures/User";
+import EventHandler from "./util/EventHandler";
+import SolaceError from "./util/SolaceError";
+
+export {
+    Client,
+    ClientOptions,
+    Constants,
+    EventHandler,
+    GatewayIntents,
+    OpCodes,
+    SolaceError,
+    SolaceIntents,
+    User,
+    WebsocketClient,
+}
