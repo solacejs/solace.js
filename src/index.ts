c52a54a1f8975ec1417845e10ee6b6e60abcc0b6
RT <rustytrim11@gmail.com>
2023-09-10 01:26:59 -0400

    Include event helper

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 import Client from "./client/Client";
-import WebsocketClient from "./client/WebsocketClient";
+import ClientOptions from "./interfaces/ClientOptions";
 import Constants from "./config/Constants";
+import Event from "./helper/Event";
+import EventHandler from "./util/EventHandler";
 import GatewayIntents from "./config/GatewayIntents";
 import OpCodes from "./config/OpCodes";
-import SolaceIntents from "./config/SolaceIntents";
-import ClientOptions from "./interfaces/ClientOptions";
-import User from "./structures/User";
-import EventHandler from "./util/EventHandler";
 import SolaceError from "./util/SolaceError";
+import SolaceIntents from "./config/SolaceIntents";
+import User from "./structures/User";
+import WebsocketClient from "./client/WebsocketClient";
 
 export {
     Client,
     ClientOptions,
     Constants,
+    Event,
     EventHandler,
     GatewayIntents,
     OpCodes,
