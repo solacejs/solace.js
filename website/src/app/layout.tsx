ddb54bbcfcf775f75da1d69bd3a6869a1a40bd0f
RT <rustytrim11@gmail.com>
2023-09-15 19:34:58 -0400

    Remove Metatdata import

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Navbar from '@/components/Navbar'
 import './globals.css'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from 'next-themes'
 
