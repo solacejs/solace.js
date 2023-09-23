df0f6e10fbd65cd8eed7f66bb81b871b2cbc7d14
RT <rustytrim11@gmail.com>
2023-09-23 17:38:15 -0400

    Make CodeBlock use MarkdownRenderer instead

--- website/src/components/CodeBlock.tsx
+++ website/src/components/CodeBlock.tsx
@@ -1,34 +1,14 @@
 "use client";
 import { useEffect, useRef } from "react"
+import MarkdownRenderer from "./MarkdownRenderer";
 
 export default function CodeBlock({ language, children }: { language?: string, children: string }) {
 
     const ref = useRef<HTMLPreElement>(null);
 
-    useEffect(() => {
-        switch (language) {
-            case "bash":
-                // eslint-disable-next-line react-hooks/exhaustive-deps
-                children = children.replace(/\$/g, `<span class="text-green-600">$</span>`);
-                break;
-            case "typescript":
-                children = children
-                    .replace(/\bimport|export|default|class|constructor|extends|from|const|let|new|public|async\b/g, `<span class='syntax-keyword'>$&</span>`)
-                    .replace(/\btrue|false\b/g, `<span class='syntax-constant'>$&</span>`)
-                    .replace(/"([^"]+)"/g, `<span class='syntax-string'>"$1"</span>`)
-                    .replace(/\/\*\*([\s\S]*?)\*\//g, `<span class='syntax-comment'>$&</span>`)
-                    .replace(/\.\w+(?![\w(])/g, `<span class='syntax-constant'>$&</span>`)
-                    .replace(/(\w+)\(/g, `<span class='syntax-entity'>$1</span>(`)
-                    .replace(/^[a-z].*\($/, `<span>test</span>`)
-                    .replace(/\b\w+:/g, `<span class='syntax-constant'>$&</span>`)
-                    .replace(/\[([^\]]+)\]\(([^)]+)\)/g, `<a href='$2'>$1</a>`)
-                break;
-        }
-
-        if (ref.current) ref.current.innerHTML = children;
-    }, []);
-
     return (
-        <pre ref={ref} className="w-fit h-fit break-word max-w-full bg-code p-4 rounded-md font-mono overflow-auto"/>
+        <MarkdownRenderer>
+            {`\`\`\`${language!}\n${children}\n\`\`\``}
+        </MarkdownRenderer>
     )
 }
