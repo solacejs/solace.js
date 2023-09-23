a8027ee867a2d097d296f26ad7d987f0a2150e00
RT <rustytrim11@gmail.com>
2023-09-23 17:37:55 -0400

    Render README from github

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,61 +1,18 @@
-"use client";
-import CodeBlock from "@/components/CodeBlock";
-import { useEffect } from "react";
+import MarkdownRenderer from "../components/MarkdownRenderer";
+import React from "react";
 
-export default function Home() {
+export default async function Home() {
 
-  useEffect(() => {
-    document.title = "Solace.js";
-  })
+    const data = await getReadMe();
 
-  return (
-    <main className='w-full h-full flex justify-center'>
-      <div className="w-full p-2 md:w-[50rem] md:p-0 h-full">
-        <h1>Solace.js</h1>
-        <p>Solace.js is an alternative to eris and discord.js, a nodejs wrapper for interfacing with discord. The goal of solace.js is to make discord bot development faster. The project structure is sort of similar to discord.js making it easy to move to. With solace.js you can get work done in a faster way.</p>
-        <h2>Installation</h2>
-        <CodeBlock language="bash">
-          {`$ npm install solace.js
-          
-$ yarn add solace.js
-          
-$ pnpm add solace.js`}
-        </CodeBlock>
+    return (
+        <div className="w-full h-full flex justify-center p-4">
+            <MarkdownRenderer className="m-4 w-full p-2 md:w-4/12 lg:w-5/12 xl:w-6/12 2xl:w-7/12 md:p-0 h-full">{data}</MarkdownRenderer>
+        </div>
+    )
+}
 
-        <h2>Usage</h2>
-        <CodeBlock language="typescript">
-          {`import { Client, Event, GatewayIntents, Registry, SolaceIntents } from "solace.js";
-import ReadyEvent from "./events/ReadyEvent";
-
-/**
-  * You can use intent groups such as SolaceIntents or just use an array of GatewayIntents
-**/
-const client = new Client({ token: "BOT_TOKEN", intents: SolaceIntents.GENERAL });
-
-const events: Event[] = [];
-
-Registry.registerEvents(client, new ReadyEvent());
-
-client.login();`}
-        </CodeBlock>
-
-        <p className="mt-2">Ready Event Example</p>
-        <CodeBlock language="typescript">
-          {`import { Client, Event } from "solace.js";
-          
-export default class ReadyEvent extends Event {
-            
-  constructor() {
-    super("ready", true);
-  }
-  
-  public async execute(client: Client) {
-    console.log(client.user!.username, "is online!");
-  }
-}`}
-        </CodeBlock>
-        <div className="h-[1rem] w-full"></div>
-      </div>
-    </main>
-  )
+const getReadMe = async () => {
+    const data = await fetch("https://raw.githubusercontent.com/solacejs/solace.js/main/README.md");
+    return await data.text();
 }
