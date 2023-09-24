55efa2655febfda451322505460b6f15043cf522
RT <rustytrim11@gmail.com>
2023-09-24 13:53:16 -0400

    Found a better way to export

--- src/index.ts
+++ src/index.ts
@@ -1,140 +1,54 @@
-import Activity from "./structures/Activity";
-import ApiActivity from "./interfaces/ApiActivity";
-import ApiApplication, { ApiApplicationInstallParams } from "./interfaces/ApiApplication";
-import ApiAttachment from "./interfaces/ApiAttachment";
-import ApiChannel from "./interfaces/ApiChannel";
-import ApiChannelCategory from "./interfaces/ApiChannelCategory";
-import ApiDmChannel from "./interfaces/ApiDmChannel";
-import ApiEmbed, { ApiEmbedAuthor, ApiEmbedField, ApiEmbedFooter, ApiEmbedImage, ApiEmbedProvider, ApiEmbedThumbnail, ApiEmbedVideo } from "./interfaces/ApiEmbed";
-import ApiEmoji from "./interfaces/ApiEmoji";
-import ApiGroupDmChannel from "./interfaces/ApiGroupDmChannel";
-import ApiGuild, { ApiGuildWelcomeScreen, ApiGuildWelcomeScreenChannel } from "./interfaces/ApiGuild";
-import ApiGuildAnnouncementChannel from "./interfaces/ApiGuildAnnouncementChannel";
-import ApiGuildMember from "./interfaces/ApiGuildMember";
-import ApiGuildTextChannel from "./interfaces/ApiGuildTextChannel";
-import ApiGuildVoiceChannel from "./interfaces/ApiGuildVoiceChannel";
-import ApiInteraction, { ApiInteractionType } from "./interfaces/ApiInteraction";
-import ApiMessage, { ApiMessageChannelMention } from "./interfaces/ApiMessage";
-import ApiReaction, { ApiReactionCountDetails } from "./interfaces/ApiReaction";
-import ApiRecipientsEntity from "./interfaces/ApiRecipientsEntity";
-import ApiRole, { ApiRoleSubscriptionData, ApiRoleTag } from "./interfaces/ApiRole";
-import ApiSticker, { ApiStickerItem } from "./interfaces/ApiSticker";
-import ApiTeam, { ApiTeamMember } from "./interfaces/ApiTeam";
-import ApiThreadChannel, { ApiThreadMetadata } from "./interfaces/ApiThreadChannel";
-import ApiUser from "./interfaces/ApiUser";
-import Application, { ApplicationInstallParams } from "./structures/Application";
-import Attachment from "./structures/Attachment";
-import Channel from "./structures/Channel";
-import Client from "./client/Client";
-import ClientEvents from "./interfaces/ClientEvents";
-import ClientOptions from "./interfaces/ClientOptions";
-import Constants from "./config/Constants";
-import Embed, { EmbedAuthor, EmbedField, EmbedFooter, EmbedImage, EmbedProvider, EmbedThumbnail, EmbedVideo } from "./structures/Embed";
-import Emoji from "./structures/Emoji";
-import Event from "./helper/Event";
-import EventHandler from "./util/EventHandler";
-import GatewayIntents from "./config/GatewayIntents";
-import Guild from "./structures/Guild";
-import GuildMember from "./structures/GuildMember";
-import Interaction from "./structures/Interaction";
-import Message from "./structures/Message";
-import OpCodes from "./config/OpCodes";
-import Plugin from "./helper/Plugin";
-import Reaction, { ReactionCountDetails, RoleSubscriptionData } from "./structures/Reaction";
-import Registry from "./helper/Registry";
-import Role from "./structures/Role";
-import SolaceError from "./util/SolaceError";
-import SolaceIntents from "./config/SolaceIntents";
-import Sticker, { StickerItem } from "./structures/Sticker";
-import Team from "./structures/Team";
-import TeamMember from "./structures/TeamMember";
-import ThreadChannel from "./structures/ThreadChannel";
-import User from "./structures/User";
-import WebsocketClient from "./client/WebsocketClient";
-import WelcomeScreen from "./structures/WelcomeScreen";
-
-export {
-    Activity,
-    ApiActivity,
-    ApiApplication,
-    ApiApplicationInstallParams,
-    ApiAttachment,
-    ApiChannel,
-    ApiChannelCategory,
-    ApiDmChannel,
-    ApiEmbed,
-    ApiEmbedAuthor,
-    ApiEmbedField,
-    ApiEmbedFooter,
-    ApiEmbedImage,
-    ApiEmbedProvider,
-    ApiEmbedThumbnail,
-    ApiEmbedVideo,
-    ApiEmoji,
-    ApiGroupDmChannel,
-    ApiGuild,
-    ApiGuildAnnouncementChannel,
-    ApiGuildMember,
-    ApiGuildTextChannel,
-    ApiGuildVoiceChannel,
-    ApiGuildWelcomeScreen,
-    ApiGuildWelcomeScreenChannel,
-    ApiInteraction,
-    ApiInteractionType,
-    ApiMessage,
-    ApiMessageChannelMention,
-    ApiReaction,
-    ApiReactionCountDetails,
-    ApiRecipientsEntity,
-    ApiRole,
-    ApiRoleSubscriptionData,
-    ApiRoleTag,
-    ApiSticker,
-    ApiStickerItem,
-    ApiTeam,
-    ApiTeamMember,
-    ApiThreadChannel,
-    ApiThreadMetadata,
-    ApiUser,
-    Application,
-    ApplicationInstallParams,
-    Attachment,
-    Channel,
-    Client,
-    ClientEvents,
-    ClientOptions,
-    Constants,
-    Embed,
-    EmbedAuthor,
-    EmbedField,
-    EmbedFooter,
-    EmbedImage,
-    EmbedProvider,
-    EmbedThumbnail,
-    EmbedVideo,
-    Emoji,
-    Event,
-    EventHandler,
-    GatewayIntents,
-    Guild,
-    GuildMember,
-    Interaction,
-    Message,
-    OpCodes,
-    Plugin,
-    Reaction,
-    ReactionCountDetails,
-    Registry,
-    Role,
-    RoleSubscriptionData,
-    SolaceError,
-    SolaceIntents,
-    Sticker,
-    StickerItem,
-    Team,
-    TeamMember,
-    ThreadChannel,
-    User,
-    WebsocketClient,
-    WelcomeScreen,
-}
+export * from "./structures/Activity";
+export * from "./interfaces/ApiActivity";
+export * from "./interfaces/ApiApplication";
+export * from "./interfaces/ApiAttachment";
+export * from "./interfaces/ApiChannel";
+export * from "./interfaces/ApiChannelCategory";
+export * from "./interfaces/ApiDmChannel";
+export * from "./interfaces/ApiEmbed";
+export * from "./interfaces/ApiEmoji";
+export * from "./interfaces/ApiGroupDmChannel";
+export * from "./interfaces/ApiGuild";
+export * from "./interfaces/ApiGuildAnnouncementChannel";
+export * from "./interfaces/ApiGuildMember";
+export * from "./interfaces/ApiGuildTextChannel";
+export * from "./interfaces/ApiGuildVoiceChannel";
+export * from "./interfaces/ApiInteraction";
+export * from "./interfaces/ApiMessage";
+export * from "./interfaces/ApiReaction";
+export * from "./interfaces/ApiRecipientsEntity";
+export * from "./interfaces/ApiRole";
+export * from "./interfaces/ApiSticker";
+export * from "./interfaces/ApiTeam";
+export * from "./interfaces/ApiThreadChannel";
+export * from "./interfaces/ApiUser";
+export * from "./structures/Application";
+export * from "./structures/Attachment";
+export * from "./structures/Channel";
+export * from "./client/Client";
+export * from "./interfaces/ClientEvents";
+export * from "./interfaces/ClientOptions";
+export * from "./config/Constants";
+export * from "./structures/Embed";
+export * from "./structures/Emoji";
+export * from "./helper/Event";
+export * from "./util/EventHandler";
+export * from "./config/GatewayIntents";
+export * from "./structures/Guild";
+export * from "./structures/GuildMember";
+export * from "./structures/Interaction";
+export * from "./structures/Message";
+export * from "./config/OpCodes";
+export * from "./helper/Plugin";
+export * from "./structures/Reaction";
+export * from "./helper/Registry";
+export * from "./structures/Role";
+export * from "./util/SolaceError";
+export * from "./config/SolaceIntents";
+export * from "./structures/Sticker";
+export * from "./structures/Team";
+export * from "./structures/TeamMember";
+export * from "./structures/ThreadChannel";
+export * from "./structures/User";
+export * from "./client/WebsocketClient";
+export * from "./structures/WelcomeScreen";
