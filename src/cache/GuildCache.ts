b21f7749a43481fdb1fdca8bfea1031b452564f8
RT <rustytrim11@gmail.com>
2023-10-15 13:18:47 -0400

    Added size to cache and return null when no guild

--- src/cache/GuildCache.ts
+++ src/cache/GuildCache.ts
@@ -1,12 +1,21 @@
+import { Client } from "../client/Client";
 import { Constants } from "../config/Constants";
 import { Guild } from "../structures/Guild";
+import { Collection } from "../util/Collection";
 import { SolaceError } from "../util/SolaceError";
-import { CacheBase } from "./CacheBase";
 
-export class GuildCache extends CacheBase {
+export class GuildCache {
 
-    public async fetch(id: string): Promise<Guild> {
-        if (this.cache.has(id)) return this.cache.get(id);
+    public readonly cache: Collection<string, Guild>;
+
+    constructor(public client: Client) {
+        let maxSize = 256;
+        if (client.options.cache) maxSize = client.options.cache.guilds ? client.options.cache.guilds : maxSize;
+        this.cache = new Collection<string, Guild>(maxSize);
+    }
+
+    public async fetch(id: string): Promise<Guild | null> {
+        if (this.cache.has(id)) return this.cache.get(id) ?? null;
 
         const res = await fetch(`${Constants.API}/guilds/${id}`, {
             headers: {
@@ -18,9 +27,8 @@ export class GuildCache extends CacheBase {
         const data = await res.json();
 
         if (data.errors) {
-            if (data.errors.guild_id) {
-                throw new SolaceError(data.message, data.errors.guild_id._errors[0].message)
-            }
+            if (data.errors.guild_id) throw new SolaceError(data.message, data.errors.guild_id._errors[0].message);
+            return null;
         }
 
         const guild = new Guild(data);
